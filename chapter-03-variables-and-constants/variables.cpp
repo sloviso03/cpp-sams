06243492893d2cc94577228798d0c98ae08a9585
sloviso03 <sloviso03@gmail.com>
2026-05-06 12:00:00 -0300

    Chapter 03: variables and constants

---
+++ chapter-03-variables-and-constants/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  unsigned short int Width = 5, Lenght;
+  Lenght = 10;
+
+  unsigned short int Area = (Width * Lenght);
+  
+  cout << "Width: " << Width << endl;
+  cout << "Lenght: " << Lenght << endl;
+  cout << "Area: " << Area << endl;
+  
+  return 0;
+}
